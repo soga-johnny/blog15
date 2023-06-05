333bdf2768fa5558f1131c5091dbe9ec3329562f
soga-johnny <soga.johnny@gmail.com>
2023-06-05 15:34:02 +0900

    passYarnbuild

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
 
   return (
   <div>
-    <h1>This is TOP page of blog15</h1>
+    <h1>This is TOP page of blog15!</h1>
     <div>
     {projects.map((project) => (
       <Link href={`/projects/${project.slug}`} key={project._id}>
