da427cc87e70f85b6c89c59ed36b23aadbae8296
soga-johnny <soga.johnny@gmail.com>
2023-06-05 18:01:31 +0900

    a

--- src/app/projects/[project]/page.tsx
+++ src/app/projects/[project]/page.tsx
@@ -9,6 +9,7 @@ type Props = {
     params: {project: string};
 };
 
+
 export default async function Project ({ params }: Props ) {
     const slug  = params.project;
     const project = await getProject(slug);
